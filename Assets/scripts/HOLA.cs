2585399d89b151373c7670da1ca6aac3335c3a22
FabsEX <f.seplvedaahumada@uandresbello.edu>
2023-11-14 18:45:03 -0300

    puto

--- Assets/scripts/HOLA.cs
+++ Assets/scripts/HOLA.cs
@@ -8,7 +8,7 @@ public class HOLA : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Hello world");
+        print("puto el que lo lea");
     }
 
     // Update is called once per frame
