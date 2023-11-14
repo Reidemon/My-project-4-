d74c9f48fa006f0ee47f407d9fd5cb7b9b78cb4b
Guidobeck <r.alegraagero@uandresbello.edu>
2023-11-14 18:29:51 -0300

    cambio de texto de madafacas a Hello worl

--- Assets/scripts/HOLA.cs
+++ Assets/scripts/HOLA.cs
@@ -8,7 +8,7 @@ public class HOLA : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("MADAFACAS");
+        print("Hello world");
     }
 
     // Update is called once per frame
