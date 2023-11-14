d295aa639911d125b3dc1734a245979931726865
Guidobeck <r.alegraagero@uandresbello.edu>
2023-11-14 18:06:57 -0300

    hola

---
+++ Assets/scripts/HOLA.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HOLA : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("MADAFACAS");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
