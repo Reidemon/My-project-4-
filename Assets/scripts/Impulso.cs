6314d509e86ebd1db82fd963cb7512d16fca1beb
FabsEX <f.seplvedaahumada@uandresbello.edu>
2023-11-21 16:18:00 -0300

    Avances

---
+++ Assets/scripts/Impulso.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Impulso : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float thrust = 2f;
+
+    private void Update()
+    {
+        
+    
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            rb.AddForce(0, 0, thrust, ForceMode.Impulse);
+        }
+    }
+
+}
