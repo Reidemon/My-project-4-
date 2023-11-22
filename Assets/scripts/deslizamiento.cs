43c3d5a5760d6fccc8500e950331f2f9f3e06d73
Guidobeck <r.alegraagero@uandresbello.edu>
2023-11-21 22:03:04 -0300

    lo segundo que pediste

--- Assets/scripts/deslizamiento.cs
+++ Assets/scripts/deslizamiento.cs
@@ -1,17 +1,23 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class deslizamiento : MonoBehaviour
 {
+   
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.CompareTag("azul"))
         {
             Destroy(gameObject);
         }
+        
+        
+    }
 
-    } 
     
+
+
 }
---
+++ Assets/scripts/destroy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject)
+        {
+            Destroy(GameObject.FindWithTag("red"));
+        }
+    }
+}
